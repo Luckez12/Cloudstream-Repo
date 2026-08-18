6de607d2b7831f79efa3afa90a7f3544cb261a96
Luckez12 <izekaiworks@gmail.com>
2026-08-18 23:02:37 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import com.android.build.gradle.BaseExtension
+import com.android.build.api.dsl.LibraryExtension
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
@@ -11,9 +11,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.android.tools.build:gradle:9.1.1")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
     }
 }
 
@@ -25,43 +25,37 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(
-    configuration: CloudstreamExtension.() -> Unit
-) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByType<CloudstreamExtension>().configuration()
 
-fun Project.android(
-    configuration: BaseExtension.() -> Unit
-) = extensions.getByName<BaseExtension>("android").configuration()
+fun Project.android(configuration: LibraryExtension.() -> Unit) =
+    extensions.getByType<LibraryExtension>().configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(
-            System.getenv("GITHUB_REPOSITORY")
-                ?: "Luckez12/Cloudstream-Repo"
-        )
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/HatsuneMikuUwU/cloudstream-extensions-uwu")
+        authors = listOf("Miku")
     }
 
     android {
-        namespace = "com.luckez12.cloudstream"
+        namespace = "com.miku"
+        compileSdk = 35
 
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
 
-        tasks.withType<KotlinJvmCompile> {
+        tasks.withType<KotlinJvmCompile>().configureEach {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
+                jvmTarget.set(JvmTarget.JVM_17)
 
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
@@ -73,23 +67,34 @@ subprojects {
     }
 
     dependencies {
-        val cloudstream by configurations
-        val implementation by configurations
-
-        // CloudStream host API stubs.
-        // This is required for Plugin, registerMainAPI and registerExtractorAPI.
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.11")
-        implementation("org.jsoup:jsoup:1.18.3")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-
-        // Keep this available for providers/extractors that rely on NewPipe.
-        implementation("com.github.teamnewpipe:NewPipeExtractor:v0.25.2")
+        add("cloudstream", "com.lagradost:cloudstream3:pre-release")
+        
+        // Kotlin & Coroutines
+        add("implementation", kotlin("stdlib"))
+        add("implementation", "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
+        add("implementation", "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+        add("implementation", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+        
+        // Network & Scraping
+        add("implementation", "com.github.Blatzar:NiceHttp:0.4.18")
+        add("implementation", "com.squareup.okhttp3:okhttp:5.4.0")
+        add("implementation", "org.jsoup:jsoup:1.22.2")
+        
+        // JSON Parsing
+        add("implementation", "com.fasterxml.jackson.module:jackson-module-kotlin:2.22.1")
+        add("implementation", "com.fasterxml.jackson.core:jackson-databind:2.22.1")
+        add("implementation", "com.google.code.gson:gson:2.14.0")
+        
+        // JavaScript Evaluation
+        add("implementation", "com.faendir.rhino:rhino-android:1.6.0")
+        add("implementation", "app.cash.quickjs:quickjs-android:0.9.2")
+        
+        // Utils & Core
+        add("implementation", "me.xdrop:fuzzywuzzy:1.4.0")
+        add("implementation", "androidx.core:core-ktx:1.19.0")
     }
 }
 
-task<Delete>("clean") {
+tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
