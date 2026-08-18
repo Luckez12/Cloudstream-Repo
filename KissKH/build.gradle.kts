bdda7e5c83e8afc8be325c1b92c3d3e1c9decbb6
Luckez12 <izekaiworks@gmail.com>
2026-08-18 18:03:42 +0800

    Add files via upload

--- KissKH/build.gradle.kts
+++ KissKH/build.gradle.kts
@@ -1,1 +1,9 @@
+version = 1
 
+cloudstream {
+    description = "KissKH"
+    authors = listOf("Luckez12")
+    language = "en"
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
+}
