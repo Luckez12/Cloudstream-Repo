350e13d7138bbd505ad2ee41a8142ddf633a4249
Luckez12 <izekaiworks@gmail.com>
2026-08-19 13:33:05 +0800

    Update build.gradle.kts

--- OneTouchTV/build.gradle.kts
+++ OneTouchTV/build.gradle.kts
@@ -6,4 +6,5 @@ cloudstream {
     authors = listOf("Luckez")
     status = 1
     tvTypes = listOf("AsianDrama", "Anime", "TvSeries", "Movie")
+    iconUrl = "https://cdn.animaapp.com/projects/6215178219a2fd7886bf5a52/releases/621519ce72dadb5503e24f9f/img/app-icon%401x.png"
 }
