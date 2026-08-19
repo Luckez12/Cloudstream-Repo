7beba1bba7b86dbecee5b31f0918111c10f521f4
Luckez12 <izekaiworks@gmail.com>
2026-08-19 09:13:50 +0800

    Add files via upload

---
+++ OneTouchTV/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    language = "en"
+    description = "OneTouchTV - Asian Drama and Anime"
+    authors = listOf("Kurato")
+    status = 1
+    tvTypes = listOf("AsianDrama", "Anime", "TvSeries", "Movie")
+}
