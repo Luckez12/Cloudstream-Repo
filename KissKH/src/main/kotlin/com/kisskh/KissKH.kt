8cc250bce1bec4f49f8c0a0bccea502d9b4c8c88
Luckez12 <izekaiworks@gmail.com>
2026-08-20 05:32:03 +0800

    Update KissKH.kt

--- KissKH/src/main/kotlin/com/kisskh/KissKH.kt
+++ KissKH/src/main/kotlin/com/kisskh/KissKH.kt
@@ -21,7 +21,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 class KissKH : MainAPI() {
     override var mainUrl = "https://kisskh.id"
-    override var name = "KissKH"
+    override var name = "KissKH 👾"
     override val hasMainPage = true
     override var lang = "en"
     override val hasQuickSearch = false
