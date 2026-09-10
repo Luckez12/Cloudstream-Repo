32541a58fdd7213273852835875c054c99091f8a
Luckez12 <izekaiworks@gmail.com>
2026-09-10 13:07:37 +0800

    Update build.gradle.kts

--- Animexin/build.gradle.kts
+++ Animexin/build.gradle.kts
@@ -6,5 +6,5 @@ cloudstream {
     authors = listOf("Luckez")
     status = 1
     tvTypes = listOf("AnimeMovie", "Anime", "Cartoon")
-    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://animexin.dev&size=%size%"
+    iconUrl = "https://www.google.com/imgres?imgurl=https://play-lh.googleusercontent.com/O1SSgHfm35FhWgX1july1MNYYzzY4h-7XFdDzcodM7R6ua3iXvci_ONv510P9d9CbIeUVY-KOyM8QGhM9vcA-Vo&imgrefurl=https://play.google.com/store/apps/details?id%3Dcom.animexin.nsbdanimeoitvanime&h=512&w=512&tbnid=WQ8y45OBA1P6PM&tbnh=225&tbnw=225&osm=1&hcb=1&source=lens-native&docid=7jng3sa6wyIKfM"
 }
