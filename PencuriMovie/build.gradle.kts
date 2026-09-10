fbca20a3d0a78e22290eda5fd7d5ca71eda3e5e7
Luckez12 <izekaiworks@gmail.com>
2026-09-10 13:03:30 +0800

    Update build.gradle.kts

--- PencuriMovie/build.gradle.kts
+++ PencuriMovie/build.gradle.kts
@@ -20,5 +20,5 @@ cloudstream {
     // You can find a list of available types here:
     // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
     tvTypes = listOf("Movie","TvSeries")
-    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://ww44.pencurimovie.baby&size=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=pencurimovie.baby&sz=256"
 }
